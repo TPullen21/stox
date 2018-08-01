ed7428aef6d03cbaa7145681b224345664781a54
Tom Pullen <tpullen21@gmail.com>
2018-08-01 13:27:54 +0100

    Added news component

---
+++ src/components/StockDetail/News/News.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './News.css'
+
+const news = (props) => {
+
+    const relativeTime = props.datetime;
+
+    let summary = props.summary;
+    let articleUrl = <a href={props.url} target="__blank">Read more</a>;
+
+    if(summary && summary.startsWith('No summ')) {
+        summary = articleUrl;
+        articleUrl = null;
+    }
+
+    return (
+        <div className="News">
+            <div className="Source">{props.source} • {relativeTime}</div>
+            <div className="Headline">{props.headline}</div>
+            <div className="Summary">{summary}</div>
+            <div className="ArticleUrl">{articleUrl}</div>
+        </div>
+    );
+
+}
+
+export default news;

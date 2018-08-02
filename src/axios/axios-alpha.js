39396c94809d20826a395f843a725ecea9c6d356
Tom Pullen <tpullen21@gmail.com>
2018-08-02 10:29:00 +0100

    Updated axios files

--- src/axios/axios-alpha.js
+++ src/axios/axios-alpha.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://www.alphavantage.co/query?apikey=0&function=TIME_SERIES_DAILY&symbol='
+    baseURL: 'https://www.alphavantage.co/query?apikey=9994A20XXTNQDRO1&function=TIME_SERIES_DAILY&symbol='
 });
 
 export default instance;
--- src/axios/axios-intrinio.js
+++ src/axios/axios-intrinio.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const username = "";
-const password = "";
+const username = "8ac127eedfb32ac45039462563d6ed00";
+const password = "b4627820484c7fa41b34250cfa29df45";
 const auth = "Basic " + new Buffer(username + ':' + password).toString('base64');
 
 const instance = axios.create({

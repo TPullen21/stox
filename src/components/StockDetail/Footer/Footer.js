20e36c180aa850b48af715a3268e54302f6a2c31
Tom Pullen <tpullen21@gmail.com>
2018-08-02 15:22:41 +0100

    Further footer work

--- src/components/StockDetail/Footer/Footer.js
+++ src/components/StockDetail/Footer/Footer.js
@@ -6,6 +6,9 @@ const footer = (props) => {
     return (
         <div className="Footer-Root">
             <div className="Footer-Name">{props.companyName}</div>
+            <div className="Footer-Button">		
+                <div onClick={this.addStockHandler}><span>Remove</span></div>		
+            </div>
             <div className="Footer-ButtonContainer"></div>
         </div>
     );
